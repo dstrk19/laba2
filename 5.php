25771a631b9603aec718a63949479be24712f0e5
covaleva.ian100 <covaleva.ian100@gmail.com>
2019-12-19 23:16:32 +0200

    Second commit

--- 5.php
+++ 5.php
@@ -8,11 +8,11 @@
 	<p>Choose your language</p>
 	<p>
 <select name=lang[] size=7 multiple>
-<option value=Hello,. $_SESSION['login']  >English</option>
-<option value=Привет, . $_SESSION['login'] >Russian</option>
-<option value=Вітаємо, . $_SESSION['login'] >Ukrainian</option>
-<option value=Ciao, . $_SESSION['login']>Italian</option>
-<option value=Halo, . $_SESSION['login'] >German</option>
+<option value=Hello,  >English</option>
+<option value=Привет,  >Russian</option>
+<option value=Вітаємо,  >Ukrainian</option>
+<option value=Ciao, >Italian</option>
+<option value=Halo,  >German</option>
 </select>
 </p>
 <hr size="1">
