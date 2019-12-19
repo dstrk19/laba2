57610e051f655b8203d5360076e658fadb66215b
covaleva.ian100 <covaleva.ian100@gmail.com>
2019-12-19 23:03:55 +0200

    first commit

--- script.php
+++ script.php
@@ -9,7 +9,7 @@ echo '<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
 echo '<title>Language</title>';
 echo '</head>';
 echo '<body>';
-foreach ($_POST['lang'] as $keys=>$values) echo "$values";
+foreach ($_POST['lang'] as $keys=>$values) echo "$values". $_SESSION['login'];
 echo '</body>';
 echo '</html>';
 ?>
